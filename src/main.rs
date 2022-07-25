d76e9ff975c2f70469ee402eb216e0150dadfc67
Martin Popovski <martinkozle@yahoo.com>
2022-07-25 02:18:17 +0200

    Simplify systems

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,47 @@
-use bevy::DefaultPlugins;
-use bevy::prelude::*;
+use bevy::{prelude::*, DefaultPlugins, window::PresentMode};
 
-#[derive(Default)]
-struct Mouse {
-    position: Vec2,
-}
+#[derive(Component)]
+struct MouseSquare;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .insert_resource(SpriteBundle {
-            sprite: Sprite {
-                color: Color::rgb(0.25, 0.25, 0.75),
-                custom_size: Some(Vec2::new(16.0, 16.0)),
-                ..default()
-            },
-            ..default()
+        .insert_resource(WindowDescriptor {
+            title: "Coding Big Time - Blue Square".to_string(),
+            width: 800.,
+            height: 600.,
+            present_mode: PresentMode::Immediate,
+            ..Default::default()
         })
         .add_startup_system(setup)
         // .add_startup_system(|mut windows: ResMut<Windows>| windows.get_primary_mut().unwrap().set_cursor_visibility(false))
         .add_system(move_mouse)
-        .insert_resource(Mouse { position: Vec2::default() })
         .run();
 }
 
 fn move_mouse(
-    mut event: EventReader<CursorMoved>,
-    mut mouse: ResMut<Mouse>,
-    mut query: Query<&mut GlobalTransform, With<Sprite>>,
-    camera: Query<&Transform, With<Camera>>,
-    windows: ResMut<Windows>,
+    windows: Res<Windows>,
+    camera_transform: Query<&Transform, (With<Camera>, Without<MouseSquare>)>,
+    mut mouse_square_transform: Query<&mut Transform, (With<MouseSquare>, Without<Camera>)>,
 ) {
-    if let Some(cursor_moved) = event.iter().last() {
-        mouse.position = cursor_moved.position;
-    }
-    for mut transform in query.iter_mut() {
-        transform.translation = window_to_world(mouse.position, windows.primary(), camera.iter().next().unwrap());
+    let window = windows.get_primary().unwrap();
+    if let Some(cursor_position) = window.cursor_position()  {
+        mouse_square_transform.single_mut().translation = window_to_world(
+            cursor_position, window, camera_transform.single(),
+        );
     }
 }
 
-fn setup(mut commands: Commands, sprite_bundle: Res<SpriteBundle>) {
+fn setup(mut commands: Commands) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-    commands.spawn_bundle(sprite_bundle.clone());
+    commands.spawn_bundle(SpriteBundle {
+        sprite: Sprite {
+            color: Color::rgb(0.25, 0.25, 0.75),
+            custom_size: Some(Vec2::new(16.0, 16.0)),
+            ..default()
+        },
+        ..default()
+    }).insert(MouseSquare);
 }
 
 fn window_to_world(
