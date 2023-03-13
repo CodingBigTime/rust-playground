0f4b0fe1d2496dfa334ef36255d84a6d5cdb4b38
Martin Popovski <martinkozle@yahoo.com>
2023-03-13 04:44:14 +0100

    Refactor whole code to use uom crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+extern crate uom;
+
 use std::time::Duration;
 
 use bevy::{
@@ -11,125 +13,49 @@ use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use bevy_prototype_lyon::{draw::Fill, entity::ShapeBundle, prelude::*, shapes::Circle};
 use bevy_rapier2d::{plugin::*, prelude::*};
 use rand::prelude::*;
+use uom::{
+    si,
+    si::{
+        area, energy, heat_capacity, length, mass, mass_density, specific_heat_capacity,
+        temperature_interval, thermal_conductance, thermal_conductivity, thermodynamic_temperature,
+        time, volume,
+    },
+};
 
-type Joules = f32;
-type JoulesPerKelvin = f32;
-type JoulesPerKelvinPerKilogram = f32;
-type WattsPerMetreKelvin = f32;
-type KiloGram = f32;
-type KiloGramPerMetreCubed = f32;
-type CubicMetres = f32;
-
-enum Temperature {
-    Kelvin(f32),
-    Celsius(f32),
-    Fahrenheit(f32),
+trait ThermodynamicTemperatureToTemperatureIntervalConversion {
+    fn as_temperature_interval(&self) -> si::f64::TemperatureInterval;
 }
 
-impl Default for Temperature {
-    fn default() -> Self {
-        Self::Kelvin(0f32)
-    }
-}
-
-impl Into<f32> for Temperature {
-    fn into(self) -> f32 {
-        match self {
-            Self::Kelvin(kelvin) => kelvin,
-            Self::Celsius(celsius) => celsius + 273.15,
-            Self::Fahrenheit(fahrenheit) => (fahrenheit - 32.0) * 5.0 / 9.0 + 273.15,
-        }
-    }
-}
-
-impl std::fmt::Display for Temperature {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::Kelvin(kelvin) => write!(f, "{}°K", kelvin),
-            Self::Celsius(celsius) => write!(f, "{}°C", celsius),
-            Self::Fahrenheit(fahrenheit) => write!(f, "{}°F", fahrenheit),
-        }
-    }
-}
-
-impl Temperature {
-    const ABSOLUTE_ZERO: Temperature = Temperature::Kelvin(0.0);
-    const WATER_MELTING_POINT: Temperature = Temperature::Celsius(0.0);
-    const WATER_BOILING_POINT: Temperature = Temperature::Celsius(100.0);
-    const HUMAN_BODY_TEMPERATURE: Temperature = Temperature::Celsius(37.0);
-    const SUN_SURFACE_TEMPERATURE: Temperature = Temperature::Celsius(5500.0);
-    const SUN_CORE_TEMPERATURE: Temperature = Temperature::Celsius(15_000_000.0);
-    const ROOM_TEMPERATURE: Temperature = Temperature::Celsius(20.0);
-
-    fn as_kelvin_f32(&self) -> f32 {
-        match self {
-            Self::Kelvin(kelvin) => *kelvin,
-            Self::Celsius(celsius) => celsius + 273.15,
-            Self::Fahrenheit(fahrenheit) => (fahrenheit - 32.0) * 5.0 / 9.0 + 273.15,
-        }
-    }
-
-    fn as_kelvin(&self) -> Temperature {
-        Self::Kelvin(self.as_kelvin_f32())
-    }
-
-    fn as_celsius_f32(&self) -> f32 {
-        match self {
-            Self::Kelvin(kelvin) => kelvin - 273.15,
-            Self::Celsius(celsius) => *celsius,
-            Self::Fahrenheit(fahrenheit) => (fahrenheit - 32.0) * 5.0 / 9.0,
-        }
-    }
-
-    fn as_celsius(&self) -> Temperature {
-        Self::Celsius(self.as_celsius_f32())
-    }
-
-    fn as_fahrenheit_f32(&self) -> f32 {
-        match self {
-            Self::Kelvin(kelvin) => (kelvin - 273.15) * 9.0 / 5.0 + 32.0,
-            Self::Celsius(celsius) => celsius * 9.0 / 5.0 + 32.0,
-            Self::Fahrenheit(fahrenheit) => *fahrenheit,
-        }
-    }
-
-    fn as_fahrenheit(&self) -> Temperature {
-        Self::Fahrenheit(self.as_fahrenheit_f32())
+impl ThermodynamicTemperatureToTemperatureIntervalConversion for si::f64::ThermodynamicTemperature {
+    fn as_temperature_interval(&self) -> si::f64::TemperatureInterval {
+        si::f64::TemperatureInterval::new::<temperature_interval::kelvin>(
+            self.get::<thermodynamic_temperature::kelvin>(),
+        )
     }
 }
 
 #[derive(Component, Reflect)]
 struct Material {
-    thermal_conductivity: WattsPerMetreKelvin,
-    specific_heat_capacity: JoulesPerKelvinPerKilogram,
-    density: KiloGramPerMetreCubed,
+    #[reflect(ignore)]
+    thermal_conductivity: si::f64::ThermalConductivity,
+    #[reflect(ignore)]
+    specific_heat_capacity: si::f64::SpecificHeatCapacity,
+    #[reflect(ignore)]
+    density: si::f64::MassDensity,
     base_color: Color,
 }
 
-impl Material {
-    const ALUMINIUM: Material = Material {
-        thermal_conductivity: 237.0,
-        specific_heat_capacity: 0.9,
-        density: 2.7,
-        base_color: Color::rgb(0.8, 0.8, 0.9),
-    };
-    const COPPER: Material = Material {
-        thermal_conductivity: 385.0,
-        specific_heat_capacity: 0.385,
-        density: 8.96,
-        base_color: Color::rgb(0.9, 0.6, 0.2),
-    };
-    const IRON: Material = Material {
-        thermal_conductivity: 80.0,
-        specific_heat_capacity: 0.45,
-        density: 7.87,
-        base_color: Color::rgb(0.8, 0.8, 0.8),
-    };
+enum MaterialType {
+    Aluminium,
+    Copper,
+    Iron,
+}
 
+impl Material {
     fn new(
-        thermal_conductivity: f32,
-        specific_heat_capacity: f32,
-        density: KiloGramPerMetreCubed,
+        thermal_conductivity: si::f64::ThermalConductivity,
+        specific_heat_capacity: si::f64::SpecificHeatCapacity,
+        density: si::f64::MassDensity,
         base_color: Color,
     ) -> Self {
         Material {
@@ -141,54 +67,104 @@ impl Material {
     }
 }
 
+impl From<MaterialType> for Material {
+    fn from(material_type: MaterialType) -> Self {
+        match material_type {
+            MaterialType::Aluminium => Material::new(
+                si::f64::ThermalConductivity::new::<thermal_conductivity::watt_per_meter_kelvin>(
+                    237.0,
+                ),
+                si::f64::SpecificHeatCapacity::new::<
+                    specific_heat_capacity::joule_per_kilogram_kelvin,
+                >(0.9),
+                si::f64::MassDensity::new::<mass_density::kilogram_per_cubic_meter>(2.7),
+                Color::rgb(0.8, 0.8, 0.9),
+            ),
+            MaterialType::Copper => Material::new(
+                si::f64::ThermalConductivity::new::<thermal_conductivity::watt_per_meter_kelvin>(
+                    385.0,
+                ),
+                si::f64::SpecificHeatCapacity::new::<
+                    specific_heat_capacity::joule_per_kilogram_kelvin,
+                >(0.385),
+                si::f64::MassDensity::new::<mass_density::kilogram_per_cubic_meter>(8.96),
+                Color::rgb(0.9, 0.6, 0.2),
+            ),
+            MaterialType::Iron => Material::new(
+                si::f64::ThermalConductivity::new::<thermal_conductivity::watt_per_meter_kelvin>(
+                    80.0,
+                ),
+                si::f64::SpecificHeatCapacity::new::<
+                    specific_heat_capacity::joule_per_kilogram_kelvin,
+                >(0.45),
+                si::f64::MassDensity::new::<mass_density::kilogram_per_cubic_meter>(7.87),
+                Color::rgb(0.8, 0.8, 0.8),
+            ),
+        }
+    }
+}
+
 #[derive(Component, Reflect)]
 struct HeatBody {
-    heat: Joules,
-    size: CubicMetres,
+    #[reflect(ignore)]
+    heat: si::f64::Energy,
+    #[reflect(ignore)]
+    volume: si::f64::Volume,
     material: Material,
 }
 
 impl HeatBody {
-    fn mass(&self) -> KiloGram {
-        self.size * self.material.density
+    fn mass(&self) -> si::f64::Mass {
+        self.volume * self.material.density
     }
 
-    fn heat_capacity(&self) -> JoulesPerKelvin {
+    fn heat_capacity(&self) -> si::f64::HeatCapacity {
         self.material.specific_heat_capacity * self.mass()
     }
 
-    fn temperature(&self) -> Temperature {
-        Temperature::Kelvin(self.heat / self.heat_capacity())
+    fn temperature(&self) -> si::f64::ThermodynamicTemperature {
+        si::f64::ThermodynamicTemperature::new::<thermodynamic_temperature::kelvin>(0.0)
+            + self.heat / self.heat_capacity()
     }
 
-    fn from_temperature_size_material(
-        temperature: Temperature,
-        size: CubicMetres,
+    fn from_temperature_volume_material(
+        temperature: si::f64::ThermodynamicTemperature,
+        volume: si::f64::Volume,
         material: Material,
     ) -> Self {
-        let heat =
-            temperature.as_kelvin_f32() * material.specific_heat_capacity * size * material.density;
+        let heat: si::f64::Energy =
+            temperature * material.specific_heat_capacity * volume * material.density;
         Self {
             heat,
-            size,
+            volume,
             material,
         }
     }
 
-    fn add_heat(&mut self, heat: Joules) {
+    fn add_heat(&mut self, heat: si::f64::Energy) {
         self.heat += heat;
     }
 
-    fn add_temperature(&mut self, temperature: Temperature) {
-        self.add_heat(temperature.as_kelvin_f32() * self.heat_capacity());
+    fn add_temperature(&mut self, temperature: si::f64::TemperatureInterval) {
+        self.add_heat(temperature * self.heat_capacity());
     }
 
     fn transfer_heat(&mut self, other: &mut Self, delta: Duration) {
-        let thermal_conductivity =
-            (self.material.thermal_conductivity + other.material.thermal_conductivity) / 2.0;
-        let heat_transfer = thermal_conductivity
-            * (self.temperature().as_kelvin_f32() - other.temperature().as_kelvin_f32())
-            * delta.as_secs_f32();
+        let time_delta: si::f64::Time = si::f64::Time::new::<time::second>(delta.as_secs_f64());
+        let temperature_delta: si::f64::TemperatureInterval =
+            self.temperature().as_temperature_interval()
+                - other.temperature().as_temperature_interval();
+        let mid_point_temperature: si::f64::ThermodynamicTemperature =
+            self.temperature() - temperature_delta / 2.0;
+        let disk_area: si::f64::Area = si::f64::Area::new::<area::square_millimeter>(1.0);
+        let disk_thickness: si::f64::Length = si::f64::Length::new::<length::millimeter>(1.0);
+        let thermal_conductance: si::f64::ThermalConductance =
+            self.material.thermal_conductivity * disk_area / disk_thickness;
+        let heat_transfer: si::f64::Energy = thermal_conductance * temperature_delta * time_delta;
+        // the heat transfer shouldn't be more than the mid point temperature
+        let heat_transfer = heat_transfer.min(mid_point_temperature * self.heat_capacity());
+        let heat_transfer = heat_transfer.max(-(mid_point_temperature * other.heat_capacity()));
+
         self.add_heat(-heat_transfer);
         other.add_heat(heat_transfer);
     }
@@ -208,16 +184,23 @@ struct PositionedParticle {
 }
 
 impl PositionedParticle {
-    fn new(x: f32, y: f32, diameter: f32, temperature: Temperature) -> Self {
+    fn new(
+        x: f32,
+        y: f32,
+        diameter: si::f64::Length,
+        temperature: si::f64::ThermodynamicTemperature,
+    ) -> Self {
         let mut rng = thread_rng();
         let angle = rng.gen_range(0.0..2. * std::f32::consts::PI);
         let dx = angle.sin() * 100.0;
         let dy = angle.cos() * 100.0;
-        let multiplier = color_multiplier(temperature.as_kelvin_f32());
-        let rgb = colortemp::temp_to_rgb(temperature.as_kelvin_f32() as i64);
+        let temperature_kelvin = temperature.get::<thermodynamic_temperature::kelvin>() as f64;
+        let diameter_millimeters = diameter.get::<length::millimeter>() as f32;
+        let multiplier = color_multiplier(temperature_kelvin as f32);
+        let rgb = colortemp::temp_to_rgb(temperature_kelvin as i64);
         Self {
             rigid_body: RigidBody::Dynamic,
-            collider: Collider::ball(diameter / 2.0 - 0.1),
+            collider: Collider::ball(diameter_millimeters / 2.0 - 0.1),
             restitution: Restitution::coefficient(1.0),
             velocity: Velocity {
                 linvel: Vec2::new(dx, dy),
@@ -227,7 +210,7 @@ impl PositionedParticle {
                 ShapeBundle {
                     path: GeometryBuilder::new()
                         .add(&Circle {
-                            radius: diameter / 2.0,
+                            radius: diameter_millimeters / 2.0,
                             ..default()
                         })
                         .build(),
@@ -240,10 +223,10 @@ impl PositionedParticle {
                     multiplier * rgb.b as f32 / 255.0,
                 )),
             ),
-            temperature: HeatBody::from_temperature_size_material(
+            temperature: HeatBody::from_temperature_volume_material(
                 temperature,
-                diameter * diameter * diameter * std::f32::consts::PI / 6.0,
-                Material::COPPER,
+                diameter * diameter * diameter * std::f64::consts::PI / 6.0,
+                Material::from(MaterialType::Copper),
             ),
             event: ActiveEvents::COLLISION_EVENTS,
         }
@@ -257,8 +240,12 @@ impl PositionedParticle {
         commands.spawn(self).insert(Sleeping::disabled());
     }
 
-    fn from_vector(position: Vec2, size: f32, temperature: Temperature) -> Self {
-        Self::new(position.x, position.y, size, temperature)
+    fn from_vector(
+        position: Vec2,
+        diameter: si::f64::Length,
+        temperature: si::f64::ThermodynamicTemperature,
+    ) -> Self {
+        Self::new(position.x, position.y, diameter, temperature)
     }
 }
 
@@ -277,8 +264,13 @@ fn setup(mut particle_counter: ResMut<ParticleCount>, mut commands: Commands) {
             ..default()
         },
     ));
-    PositionedParticle::new(0.0, 200.0, 32.0, Temperature::Kelvin(1000.0))
-        .spawn_with_sleep_disabled(&mut commands);
+    PositionedParticle::new(
+        0.0,
+        200.0,
+        si::f64::Length::new::<length::millimeter>(32.0),
+        si::f64::ThermodynamicTemperature::new::<thermodynamic_temperature::kelvin>(1000.0),
+    )
+    .spawn_with_sleep_disabled(&mut commands);
     particle_counter.0 += 1;
 
     /* Create the ground. */
@@ -327,11 +319,15 @@ fn mouse_button_events(
         for _ in 0..particles.0 {
             PositionedParticle::from_vector(
                 world_position,
-                thread_rng().gen_range(1..16) as f32,
+                si::f64::Length::new::<length::millimeter>(thread_rng().gen_range(1..16) as f64),
                 if mouse_input.pressed(MouseButton::Left) {
-                    Temperature::Kelvin(thread_rng().gen_range(0.0..6000.0))
+                    si::f64::ThermodynamicTemperature::new::<thermodynamic_temperature::kelvin>(
+                        thread_rng().gen_range(0.0..6000.0),
+                    )
                 } else {
-                    Temperature::Kelvin(thread_rng().gen_range(10000.0..1000000.0))
+                    si::f64::ThermodynamicTemperature::new::<thermodynamic_temperature::kelvin>(
+                        thread_rng().gen_range(10000.0..100000.0),
+                    )
                 },
             )
             .spawn_with_sleep_disabled(&mut commands);
@@ -358,7 +354,7 @@ fn heat_transfer_event(
     mut query: Query<(&mut HeatBody, &mut Fill)>,
 ) {
     for event in collision_events.iter() {
-        if let CollisionEvent::Started(a, b, flags) = event {
+        if let CollisionEvent::Started(a, b, _flags) = event {
             if !query.contains(*a) || !query.contains(*b) {
                 continue;
             }
@@ -367,19 +363,39 @@ fn heat_transfer_event(
             let (mut heat_component_b, mut fill_b) = entity_b;
 
             println!(
-                "Before: {} {}",
-                heat_component_a.temperature(),
-                heat_component_b.temperature()
+                "Before: temps: {} {}, heat: {} {}",
+                heat_component_a
+                    .temperature()
+                    .get::<thermodynamic_temperature::kelvin>(),
+                heat_component_b
+                    .temperature()
+                    .get::<thermodynamic_temperature::kelvin>(),
+                heat_component_a.heat.get::<energy::joule>(),
+                heat_component_b.heat.get::<energy::joule>()
             );
             heat_component_a
                 .transfer_heat(&mut heat_component_b, Duration::from_secs_f32(1.0 / 144.0));
 
-            let rgb_a =
-                colortemp::temp_to_rgb(heat_component_a.temperature().as_kelvin_f32() as i64);
-            let rgb_b =
-                colortemp::temp_to_rgb(heat_component_b.temperature().as_kelvin_f32() as i64);
-            let multiplier_a = color_multiplier(heat_component_a.temperature().as_kelvin_f32());
-            let multiplier_b = color_multiplier(heat_component_b.temperature().as_kelvin_f32());
+            let rgb_a = colortemp::temp_to_rgb(
+                heat_component_a
+                    .temperature()
+                    .get::<thermodynamic_temperature::kelvin>() as i64,
+            );
+            let rgb_b = colortemp::temp_to_rgb(
+                heat_component_b
+                    .temperature()
+                    .get::<thermodynamic_temperature::kelvin>() as i64,
+            );
+            let multiplier_a = color_multiplier(
+                heat_component_a
+                    .temperature()
+                    .get::<thermodynamic_temperature::kelvin>() as f32,
+            );
+            let multiplier_b = color_multiplier(
+                heat_component_b
+                    .temperature()
+                    .get::<thermodynamic_temperature::kelvin>() as f32,
+            );
             fill_a.color = Color::rgb(
                 multiplier_a * rgb_a.r as f32 / 255.0,
                 multiplier_a * rgb_a.g as f32 / 255.0,
@@ -392,9 +408,15 @@ fn heat_transfer_event(
             );
 
             println!(
-                "After: {} {}",
-                heat_component_a.temperature(),
-                heat_component_b.temperature()
+                "After: temps: {} {}, heat: {} {}",
+                heat_component_a
+                    .temperature()
+                    .get::<thermodynamic_temperature::kelvin>(),
+                heat_component_b
+                    .temperature()
+                    .get::<thermodynamic_temperature::kelvin>(),
+                heat_component_a.heat.get::<energy::joule>(),
+                heat_component_b.heat.get::<energy::joule>()
             );
         }
     }
